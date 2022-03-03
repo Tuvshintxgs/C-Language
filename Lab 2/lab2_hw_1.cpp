f7cdc641cb0a37da2126e73f7284636df24a42ea
Tuvshintxgs <100715053+Tuvshintxgs@users.noreply.github.com>
2022-03-04 04:35:13 +0800

    Add files via upload

--- Lab 2/lab2_hw_1.cpp
+++ Lab 2/lab2_hw_1.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, n, x;
-	printf("shalgah 4 orontoi toogoo oruulna uu: ");
-	scanf("%d", &n);
+	int d1, d2, d3, d4, n, x;
+		printf("Shalgah 4 orontoi toogoo oruulna uu: ");
+		scanf("%d", &n);
+		
+		d1=n/1000;
+		d2=n/100%10;
+		d3=n/10%10;
+		d4=n%10;
+		
+		x = (d4*1000) + (d3*100) + (d2*10) + d1;
+		
+		printf("%d %d %d %d \n", d1, d2, d3, d4);
 	
-	d = n / 1000;
-	c = n / 100 % 10;
-	b = n / 10 % 10;
-	a = n % 10;
+	(x==n) && printf("Palindrom") || printf("Palindrom bish");
 	
-	x = (a * 1000) + (b * 100) + (c * 10) + d;
-	
-	(x == n) && printf("1");
-	(x != n) && printf("0");
-	
+	return 0;
 }
--- Lab 2/lab2_hw_3.cpp
+++ Lab 2/lab2_hw_3.cpp
@@ -3,11 +3,16 @@
 int main()
 {
 	float x;
-	
+
 	printf("Avsan onoogoo oruulna uu? (0-35): ");
 	scanf("%f", &x);
-	
-	(35*0.6)>x && printf("F") || (35*0.7)>x && printf("D") || (35*0.8)>x && printf("C") || (35*0.9)>x && printf("B") || (35*1.0)>=x && printf("A");
+
+	(35*0.6)>x && printf("F")
+	|| (35*0.7)>x && printf("D")
+	||(35*0.8)>x && printf("C") 
+	|| (35*0.9)>x && printf("B")
+	|| (35*1.0)>=x && printf("A")
+	|| x>35 && printf("Ta buruu onoo oruulsan baina");
 	
 	return 0;
 }
